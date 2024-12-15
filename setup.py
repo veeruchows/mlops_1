a80246d38b603102bbfbd062a72efac10d51bc86
veeru <singuveeru101@gmail.com>
2024-12-15 14:57:34 -0500

    cc

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="veeru",
+    author_email="singuveeru101@gmail.com",
+    packages=find_packages()
+)
